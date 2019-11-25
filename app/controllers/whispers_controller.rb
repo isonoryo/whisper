0e8b788327d19ecfe094420183016741d5a4be63
isonoryo <ljiye2422@gmail.com>
2019-11-25 16:27:27 +0900

    新規投稿画面の作成

--- app/controllers/whispers_controller.rb
+++ app/controllers/whispers_controller.rb
@@ -1,4 +1,13 @@
 class WhispersController < ApplicationController
   def index
   end
+
+  def new
+    @whisper = Whisper.new
+  end
+
+  def create
+    Whisper.create(content: params[:whisper][:content])
+    redirect_to new_whisper_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get '/whispers', to: 'whispers#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :whispers
 end
